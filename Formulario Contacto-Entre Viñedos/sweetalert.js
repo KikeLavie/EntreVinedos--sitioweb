1ab9381124b690d3c1cd047c158ed5f418dfbe17
kikelavie <martinconti.90@gmail.com>
2022-05-24 19:37:28 -0300

    mi primer commit en entreviñedos

---
+++ Formulario Contacto-Entre Viñedos/sweetalert.js
@@ -0,0 +1,11 @@
+
+const boton= document.querySelector("#formulario");
+boton.addEventListener("submit");
+
+
+Swal.fire({ 
+  tittle:'Buen trabajo!',
+  text:'Nos contactaremos a la brevedad!',
+    confirmButtonText: "Aceptar",
+
+   })
